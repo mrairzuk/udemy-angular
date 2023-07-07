8496e77b0e92b47bb568d5f33a266370b9aa129c
Jevgenijs Grigorjevs <jevgenijs.grigorjevs@tietoevry.com>
2023-07-07 13:42:27 +0300

    Finish section 3 (The basics)

---
+++ ShoppingRecepie/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is a simply a test', 'https://th.bing.com/th/id/R.d6d0377ca78d2533af58263f4bec0ff6?rik=rpyXTij%2bfTMTTQ&riu=http%3a%2f%2fouritaliantable.com%2fwp-content%2fuploads%2f2010%2f06%2fcrabmeat-pasta.jpg&ehk=EifnzejPe1GBTonvlkdik%2baqdwgigs9bmYZK%2bKGwPm4%3d&risl=&pid=ImgRaw&r=0'),
+    new Recipe('A test Recipe', 'This is a simply a test', 'https://th.bing.com/th/id/R.d6d0377ca78d2533af58263f4bec0ff6?rik=rpyXTij%2bfTMTTQ&riu=http%3a%2f%2fouritaliantable.com%2fwp-content%2fuploads%2f2010%2f06%2fcrabmeat-pasta.jpg&ehk=EifnzejPe1GBTonvlkdik%2baqdwgigs9bmYZK%2bKGwPm4%3d&risl=&pid=ImgRaw&r=0'),
+    new Recipe('A test Recipe', 'This is a simply a test', 'https://th.bing.com/th/id/R.d6d0377ca78d2533af58263f4bec0ff6?rik=rpyXTij%2bfTMTTQ&riu=http%3a%2f%2fouritaliantable.com%2fwp-content%2fuploads%2f2010%2f06%2fcrabmeat-pasta.jpg&ehk=EifnzejPe1GBTonvlkdik%2baqdwgigs9bmYZK%2bKGwPm4%3d&risl=&pid=ImgRaw&r=0')
+  ];
+
+}
+ 
