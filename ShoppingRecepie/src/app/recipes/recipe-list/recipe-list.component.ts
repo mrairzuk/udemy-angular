5bb2649d3dca64d3a20f82739d1b9123a09619ff
Jevgenijs Grigorjevs <jevgenijs.grigorjevs@tietoevry.com>
2023-07-11 13:56:07 +0300

    Finish section 6

--- ShoppingRecepie/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ShoppingRecepie/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,11 +7,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is a simply a test', 'https://th.bing.com/th/id/R.d6d0377ca78d2533af58263f4bec0ff6?rik=rpyXTij%2bfTMTTQ&riu=http%3a%2f%2fouritaliantable.com%2fwp-content%2fuploads%2f2010%2f06%2fcrabmeat-pasta.jpg&ehk=EifnzejPe1GBTonvlkdik%2baqdwgigs9bmYZK%2bKGwPm4%3d&risl=&pid=ImgRaw&r=0'),
-    new Recipe('A test Recipe', 'This is a simply a test', 'https://th.bing.com/th/id/R.d6d0377ca78d2533af58263f4bec0ff6?rik=rpyXTij%2bfTMTTQ&riu=http%3a%2f%2fouritaliantable.com%2fwp-content%2fuploads%2f2010%2f06%2fcrabmeat-pasta.jpg&ehk=EifnzejPe1GBTonvlkdik%2baqdwgigs9bmYZK%2bKGwPm4%3d&risl=&pid=ImgRaw&r=0'),
-    new Recipe('A test Recipe', 'This is a simply a test', 'https://th.bing.com/th/id/R.d6d0377ca78d2533af58263f4bec0ff6?rik=rpyXTij%2bfTMTTQ&riu=http%3a%2f%2fouritaliantable.com%2fwp-content%2fuploads%2f2010%2f06%2fcrabmeat-pasta.jpg&ehk=EifnzejPe1GBTonvlkdik%2baqdwgigs9bmYZK%2bKGwPm4%3d&risl=&pid=ImgRaw&r=0')
+    new Recipe('A test Recipe1', 'This is a simply a test', 'https://th.bing.com/th/id/R.d6d0377ca78d2533af58263f4bec0ff6?rik=rpyXTij%2bfTMTTQ&riu=http%3a%2f%2fouritaliantable.com%2fwp-content%2fuploads%2f2010%2f06%2fcrabmeat-pasta.jpg&ehk=EifnzejPe1GBTonvlkdik%2baqdwgigs9bmYZK%2bKGwPm4%3d&risl=&pid=ImgRaw&r=0'),
+    new Recipe('A test Recipe2', 'This is a simply a test', 'https://th.bing.com/th/id/R.d6d0377ca78d2533af58263f4bec0ff6?rik=rpyXTij%2bfTMTTQ&riu=http%3a%2f%2fouritaliantable.com%2fwp-content%2fuploads%2f2010%2f06%2fcrabmeat-pasta.jpg&ehk=EifnzejPe1GBTonvlkdik%2baqdwgigs9bmYZK%2bKGwPm4%3d&risl=&pid=ImgRaw&r=0'),
+    new Recipe('A test Recipe3', 'This is a simply a test', 'https://th.bing.com/th/id/R.d6d0377ca78d2533af58263f4bec0ff6?rik=rpyXTij%2bfTMTTQ&riu=http%3a%2f%2fouritaliantable.com%2fwp-content%2fuploads%2f2010%2f06%2fcrabmeat-pasta.jpg&ehk=EifnzejPe1GBTonvlkdik%2baqdwgigs9bmYZK%2bKGwPm4%3d&risl=&pid=ImgRaw&r=0')
   ];
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
+  }
 }
  
--- ShoppingRecepie/src/app/recipes/recipes.component.ts
+++ ShoppingRecepie/src/app/recipes/recipes.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
+import { Recipe } from "./recipe.model";
 
 @Component({
     selector: 'app-recipes',
     templateUrl: './recipes.component.html',
 })
 export class RecipesComponent {
-
+    selectedRecipe: Recipe
 }
